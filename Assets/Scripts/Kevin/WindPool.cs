1c82d4eee050f90a499d36d84e41095aa2e32a0a
Neluxer <kevinpistall@gmail.com>
2025-05-14 17:51:34 +0100

    wind finish

--- Assets/Scripts/Kevin/WindPool.cs
+++ Assets/Scripts/Kevin/WindPool.cs
@@ -1,7 +1,11 @@
 using System.Collections;
 using UnityEngine;
 
+<<<<<<< Updated upstream
 public class WindPool : MonoBehaviour
+=======
+public class ParticleController : MonoBehaviour
+>>>>>>> Stashed changes
 {
     public float intervalo = 10f;        // Tiempo entre cada ciclo completo (debe ser mayor o igual a duracionActiva)
     public float duracionActiva = 9f;    // Tiempo que cada partícula se mantiene activa (Duration + StartLifetime)
