3cc96645a00f0559a371c48ed49da195ed4a7d7b
Neluxer <kevinpistall@gmail.com>
2025-05-21 18:25:24 +0100

    Transicion Creditos

---
+++ Assets/Scripts/Kevin/CreditsTransition.cs
@@ -0,0 +1,53 @@
+using Unity.XR.CoreUtils;
+using UnityEngine;
+
+public class CreditsTransition : MonoBehaviour
+{
+    public Transform MenuPosition, CreditPosition;
+    public Camera _camera;
+    public GameObject UiCredits1, UICredits2;
+    private void Awake()
+    {
+        _camera = Camera.main;
+        UiCredits1.SetActive(false);
+        UICredits2.SetActive(false);
+    }
+
+    public void TransitionCredits()
+    {
+        Fade.Out().AddOnFadeOutComplete((Fade fade) =>
+        {
+            XROrigin rig = FindObjectOfType<XROrigin>();
+            if (rig != null)
+            {
+                rig.transform.position = CreditPosition.position;
+                rig.transform.rotation = CreditPosition.rotation;
+                UiCredits1.SetActive(true);
+                UICredits2.SetActive(true);
+            }
+
+            // Iniciar el fade in después de mover
+            fade.In();
+        });
+    }
+    public void TransitionMenu()
+    {
+        {
+            Fade.Out().AddOnFadeOutComplete((Fade fade) =>
+            {
+                XROrigin rig = FindObjectOfType<XROrigin>();
+                if (rig != null)
+                {
+                    rig.transform.position = MenuPosition.position;
+                    rig.transform.rotation = MenuPosition.rotation;
+                    UiCredits1.SetActive(false);
+                    UICredits2.SetActive(false);
+                }
+
+
+                // Iniciar el fade in después de mover
+                fade.In();
+            });
+        }
+    }
+}
