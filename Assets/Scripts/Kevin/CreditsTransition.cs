315d4e17084d5b62c613d5a9a54ebe4cca278941
Neluxer <kevinpistall@gmail.com>
2025-05-22 22:40:27 +0100

    Creditos modificados
    Leve cambio al script de los creditos para que funcione con la ui ya creada de los logos

--- Assets/Scripts/Kevin/CreditsTransition.cs
+++ Assets/Scripts/Kevin/CreditsTransition.cs
@@ -5,12 +5,13 @@ public class CreditsTransition : MonoBehaviour
 {
     public Transform MenuPosition, CreditPosition;
     public Camera _camera;
-    public GameObject UiCredits1, UICredits2;
+    public GameObject UiCredits1, UICredits2, UICredits3;
     private void Awake()
     {
         _camera = Camera.main;
         UiCredits1.SetActive(false);
         UICredits2.SetActive(false);
+        UICredits3.SetActive(false);
     }
 
     public void TransitionCredits()
@@ -24,6 +25,7 @@ public class CreditsTransition : MonoBehaviour
                 rig.transform.rotation = CreditPosition.rotation;
                 UiCredits1.SetActive(true);
                 UICredits2.SetActive(true);
+                UICredits3.SetActive(true);
             }
 
             // Iniciar el fade in después de mover
@@ -42,6 +44,7 @@ public class CreditsTransition : MonoBehaviour
                     rig.transform.rotation = MenuPosition.rotation;
                     UiCredits1.SetActive(false);
                     UICredits2.SetActive(false);
+                    UICredits3.SetActive(false);
                 }
 
 
