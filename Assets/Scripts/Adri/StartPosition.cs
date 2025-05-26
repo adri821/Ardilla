986b5f28f531e66f2a48ab754c2e51faaea7e172
adri821 <adrianrodrigueznaranjo@alumno.ieselrincon.es>
2025-05-26 16:00:25 +0100

    fix 3

--- Assets/Scripts/Adri/StartPosition.cs
+++ Assets/Scripts/Adri/StartPosition.cs
@@ -3,10 +3,27 @@ using UnityEngine;
 
 public class StartPosition : MonoBehaviour
 {
-    public Vector3 startPosition;
-    public XROrigin origin;
+    public XROrigin xrOrigin;
+    public Transform puntoInicioDeseado; 
 
-    void Start() {
-        origin.MoveCameraToWorldLocation(startPosition);
+    void Start()
+    {
+        Recentrar();
+    }
+
+    void Recentrar()
+    {
+        Transform cameraTransform = xrOrigin.Camera.transform;
+
+        Vector3 offset = cameraTransform.position - xrOrigin.transform.position;
+        Vector3 offsetXZ = new Vector3(offset.x, 0, offset.z);
+
+        xrOrigin.transform.position = puntoInicioDeseado.position - offsetXZ;
+
+        float anguloDeseadoY = puntoInicioDeseado.eulerAngles.y;
+        float anguloCamaraY = cameraTransform.eulerAngles.y;
+        float diferenciaRotacionY = anguloDeseadoY - anguloCamaraY;
+
+        xrOrigin.transform.Rotate(0, diferenciaRotacionY, 0);
     }
 }
