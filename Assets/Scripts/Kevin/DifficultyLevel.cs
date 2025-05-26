9602fc529aeb8ffbd65b873c30c63fc81d2360fc
adri821 <adrianrodrigueznaranjo@alumno.ieselrincon.es>
2025-05-26 18:21:27 +0100

    difficultylevel update

--- Assets/Scripts/Kevin/DifficultyLevel.cs
+++ Assets/Scripts/Kevin/DifficultyLevel.cs
@@ -2,6 +2,21 @@ using UnityEngine;
 
 public class DifficultyLevel : MonoBehaviour
 {
-    public static int[] levelDificulty = {1,2};
+    public static DifficultyLevel instance;
+
     public static bool levelHard = false;
+
+    private void Awake() {
+        if (instance == null) instance = this;
+        else { Destroy(gameObject); return; }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void DificultyNormal() {
+        levelHard = false;
+    }
+
+    public void DificultyHard() {
+        levelHard = true; 
+    }
 }
